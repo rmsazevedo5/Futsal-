ebbb23b6fd2ab1438319747dded08914dcdee70d
rmsazevedo5 <rmsazevedo5@gmail.com>
2026-09-13 00:39:55 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='modicus-performance-v21-rules';
+const CACHE='modicus-performance-v22-zerozero';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
@@ -7,11 +7,11 @@ self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
   if(u.pathname.endsWith('/index.html')||u.pathname.endsWith('/')){
     e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
-      const copy=r.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return r;
+      const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;
     }).catch(()=>caches.match('./index.html')));
     return;
   }
   e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+    const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
   }).catch(()=>caches.match(e.request)));
 });
